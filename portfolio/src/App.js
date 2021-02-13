bdb4270052e10eebe263e3346a6b8d26e1df4449
Bobbi Tarkany <bl.tarkany@gmail.com>
2021-02-13 12:32:41 -0800

    added react router dom imports to App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
 import Container from '@material-ui/core/Container';
 import './App.css';
 
 function App() {
   return (
     <Container maxWidth='lg'>
-      
+
     </Container>
   );
 }
