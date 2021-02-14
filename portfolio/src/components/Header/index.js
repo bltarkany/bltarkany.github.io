70dc10faca6fc737968e4f016aadd17233295ad6
Bobbi Tarkany <bl.tarkany@gmail.com>
2021-02-14 13:23:48 -0800

    added usestyle process

--- portfolio/src/components/Header/index.js
+++ portfolio/src/components/Header/index.js
@@ -3,8 +3,16 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 
+// create style classes
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
 
-export default function Header({onClick}) {
+}));
+
+
+export default function Header({ onClick }) {
     // header build with side drawer trigger
     return (
         <header>
