5cb648baf8baffd31b2869b71e7de1cd8edb3432
Bobbi Tarkany <bl.tarkany@gmail.com>
2021-02-14 13:22:38 -0800

    added imports into header component

---
+++ portfolio/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+
+export default function Header({onClick}) {
+    // header build with side drawer trigger
+    return (
+        <header>
+
+        </header>
+    )
+
+}
