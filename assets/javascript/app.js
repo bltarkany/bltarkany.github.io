b7d2da51f5585058260660bcdab3a4e52db74c43
Bobbi Tarkany <bl.tarkany@gmail.com>
2021-02-21 09:18:30 -0800

    added old files into new branch

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+// parallax initiation
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+});
