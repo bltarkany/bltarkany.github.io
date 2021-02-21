ab518abf991b5821a2d17a6a54d00bbad145c8ad
Bobbi Tarkany <bl.tarkany@gmail.com>
2021-02-20 22:31:31 -0800

    finalized icons color and style and placement

--- portfolio/src/components/ProLinks/index.js
+++ portfolio/src/components/ProLinks/index.js
@@ -11,25 +11,25 @@ const useStyles = makeStyles((theme) => ({
         },
     },
     icons: {
-        color: '#FF3E41'
-    }
+        color: '#FF3E41',
+        fontSize: '2.8rem',
+    }, 
 }))
 
 export default function ProLinks() {
     // add use styles?
     const classes = useStyles();
-    const preventDefault = (event) => event.preventDefault();
 
     return (
-        <Typography className={`${classes.root} ${classes.icons}`} underline='none'>
-            <Link href="https://www.linkedin.com/in/bobbi-tarkany/" onClick={preventDefault}>
+        <Typography className={`${classes.root} ${classes.icons}`} underline='none' align='center'>
+            <Link href="https://www.linkedin.com/in/bobbi-tarkany/" target='_blank' color="inherit">
                 <i class="fab fa-linkedin"></i>
             </Link>
-            <Link href="https://github.com/bltarkany" onClick={preventDefault} color="inherit">
+            <Link href="https://github.com/bltarkany" target='_blank' color="inherit">
                 <i class="fab fa-github-square"></i>
             </Link>
-            <Link href="https://drive.google.com/file/d/1ttonDuFayhPhww-ePVTzQWryl0ayu1Y3/view?usp=sharing" onClick={preventDefault} variant="body2">
-                <i class="fad fa-file-user"></i>
+            <Link href="https://drive.google.com/file/d/1ttonDuFayhPhww-ePVTzQWryl0ayu1Y3/view?usp=sharing" target='_blank' color="inherit">
+                <i class="fad fa-file-pdf"></i>
             </Link>
         </Typography>
     )
