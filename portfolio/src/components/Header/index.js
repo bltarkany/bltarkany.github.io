b09f89467086b44133ac282f41e64ca5e803d4e4
Bobbi Tarkany <bl.tarkany@gmail.com>
2021-02-25 10:51:55 -0800

    removed comments and unused imports

--- portfolio/src/components/Header/index.js
+++ portfolio/src/components/Header/index.js
@@ -2,13 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Drawer from '@material-ui/core/Drawer';
-import Link from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import { spacing } from '@material-ui/system';
 
@@ -32,32 +25,6 @@ const useStyles = makeStyles((theme) => ({
     }
 
 }));
-
-// temporary drawer state
-// export default function TemporaryDrawer() {
-//     // classes for direction of the drawer
-//     const classes = useStyles();
-//     const [state, setState] = React.useState({
-//         left: false,
-//     });
-
-//     // toggle the drawer
-//     const toggleDrawer = (anchor, open) => (event) => {
-//         // handling edge cases
-//         if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-//             return;
-//         }
-//         setState({ ...state, [anchor]: open });
-//     };
-
-//     // nav list items with 
-//     // const list = (anchor) => (
-//     //     <div className={ } role='presentation' onClick={toggleDrawer(anchor, false)} onKeyDown={toggleDrawer(anchor, false)}>
-
-//     //     </div>
-//     // )
-// }
-
 
 
 export default function Header() {
