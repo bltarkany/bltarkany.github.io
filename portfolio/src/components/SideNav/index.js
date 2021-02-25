e9c60b886ac083bd1e39d73385a1d123b5c8d091
Bobbi Tarkany <bl.tarkany@gmail.com>
2021-02-25 10:17:04 -0800

    move the temp drawer componenet into side nav

--- portfolio/src/components/SideNav/index.js
+++ portfolio/src/components/SideNav/index.js
@@ -9,3 +9,29 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Drawer from '@material-ui/core/Drawer';
 import Link from 'react-router-dom'; 
+
+
+// temporary drawer state
+export default function SideNav() {
+    // classes for direction of the drawer
+    const classes = useStyles();
+    const [state, setState] = React.useState({
+        left: false,
+    });
+
+    // toggle the drawer
+    const toggleDrawer = (anchor, open) => (event) => {
+        // handling edge cases
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+        setState({ ...state, [anchor]: open });
+    };
+
+    // nav list items with 
+    const list = (anchor) => (
+        <div className={ } role='presentation' onClick={toggleDrawer(anchor, false)} onKeyDown={toggleDrawer(anchor, false)}>
+
+        </div>
+    )
+}
