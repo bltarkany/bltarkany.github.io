66f5a6df5a8bb1c46f48a3076fc6d97e3c9f9383
Bobbi Tarkany <bl.tarkany@gmail.com>
2021-02-25 10:02:50 -0800

    added imports needed from material ui

--- portfolio/src/components/Header/index.js
+++ portfolio/src/components/Header/index.js
@@ -3,6 +3,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Drawer from '@material-ui/core/Drawer';
 
 
 // create style classes
