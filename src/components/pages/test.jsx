c98ee268a83d8e6cfae55d154dfb055ae70c5f07
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-01-20 00:32:41 -0800

    updated test page for fonts

---
+++ src/components/pages/test.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import BasicCard from "../blocks/cards/basicCard";
+
+export default function FonTTest() {
+  const fonts = [
+    "arima",
+    "cormorant",
+    "cormorantg",
+    "instrument",
+    "kaushan",
+    "merienda",
+    "montserrat",
+    "shadows",
+    "zeyada",
+  ];
+
+  return <></>;
+}
