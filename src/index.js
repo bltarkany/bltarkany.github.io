21b71447d53f64c3890f8598b6eae0683fa924db
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-01-20 02:21:47 -0800

    created hash router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// pages
+import ErrorPage from "./components/pages/error";
+import Bio from "./components/pages/bio";
+import FonTTest from "./components/pages/test";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// react router dom hash router
+import { createHashRouter, RouterProvider } from "react-router-dom";
+
+// create routes
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Bio />,
+      },
+      {
+        path: "test",
+        element: <FonTTest />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
