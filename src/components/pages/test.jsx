2601487092f8b0d15b48cb640c6416c48b6f4cdc
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-01-20 02:20:41 -0800

    changed output variants

--- src/components/pages/test.jsx
+++ src/components/pages/test.jsx
@@ -1,18 +1,54 @@
 import React from "react";
-import BasicCard from "../blocks/cards/basicCard";
+import FontCard from "../blocks/cards/fontCard";
+import Grid from "@mui/material/Grid";
 
 export default function FonTTest() {
   const fonts = [
     "arima",
-    "cormorant",
-    "cormorantg",
     "instrument",
     "kaushan",
     "merienda",
-    "montserrat",
     "shadows",
     "zeyada",
   ];
 
-  return <></>;
+  const base = ["cormorantg", "montserrat", "cormorant"];
+
+  const corClasses = fonts.map((font) => ({ base: base[2], highlight: font }));
+
+  const corgClasses = fonts.map((font) => ({ base: base[0], highlight: font }));
+
+  const montClasses = fonts.map((font) => ({ base: base[1], highlight: font }));
+
+  return (
+    <Grid container spacing={2}>
+      {corClasses.map((font, i) => (
+        <Grid item xs={4}>
+          <FontCard
+            base={font.base}
+            highlight={font.highlight}
+            key={`${font.base}-i`}
+          />
+        </Grid>
+      ))}
+      {corgClasses.map((font, i) => (
+        <Grid item xs={4}>
+          <FontCard
+            base={font.base}
+            highlight={font.highlight}
+            key={`${font.base}-i`}
+          />
+        </Grid>
+      ))}
+      {montClasses.map((font, i) => (
+        <Grid item xs={4}>
+          <FontCard
+            base={font.base}
+            highlight={font.highlight}
+            key={`${font.base}-i`}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
